a65f35f1a3749515f35c153e5a2335bde8d519c0
shyjusubramanian <shyju.subramanian@gmail.com>
2017-10-13 19:47:36 +1100

    first commit for node project

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('makeUpper',(text)=>{
+    return text.toUpperCase();
+});
+
+app.set('view engine','HBS');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var baseurl = req.baseUrl;
+    //console.log(`Access baseurl ${req.url} at ${now}`);
+    var log = `Access baseurl ${req.url} at ${now}`;
+    fs.appendFile('server.log', log+'\n',(err)=>{
+        if(err){
+            console.log('error has occurred during logging ');
+        }
+    });
+    next();
+});
+
+app.get("/",(req, response)=>{
+
+    //response.send("Hello this is for testing");
+    // response.send(
+    //     {
+    //         Name: "Shyju",
+    //         likes: [
+    //             'bikes',
+    //             'books',
+    //             'Sydney'
+    //         ]
+    //     }
+    // );
+
+    response.render('home.hbs',{
+        titleBar: 'Server Home',
+        currentYear: new Date().getFullYear(),
+        pageTitle: 'This is a home page.'
+    });
+
+});
+
+app.get("/about",(req,res)=>{
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get("/bad",(req,res)=>{
+
+    res.send({
+        Error: "Unable to process this request"
+    });
+});
+
+
+app.listen(3000,()=>{
+    console.log('server started on port 3000...')
+});
